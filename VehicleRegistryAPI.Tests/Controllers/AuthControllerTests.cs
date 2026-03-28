5feab7693272aea80533e430bc11989c2f51f028
ElvisNS <elvisantonio30@gmail.com>
2026-03-28 08:15:30 -0400

    inicio de tests de controladores

---
+++ VehicleRegistryAPI.Tests/Controllers/AuthControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Logging;
+using Moq;
+using VehicleRegistryAPI.Controllers;
+using VehicleRegistryAPI.DTOS.Auth;
+using VehicleRegistryAPI.Services.Auth;
+
+namespace VehicleRegistryAPI.Tests.Controllers
+{
+    public class AuthControllerTests
+    {
+        private readonly Mock<IAuthService> _authServiceMock;
+        private readonly Mock<IValidator<LoginDto>> _validatorMock;
+        private readonly Mock<ILogger<AuthController>> _loggerMock;
+        private readonly AuthController _controller;
+
+        public AuthControllerTests()
+        {
+            _authServiceMock = new Mock<IAuthService>();
+            _validatorMock = new Mock<IValidator<LoginDto>>();
+            _loggerMock = new Mock<ILogger<AuthController>>();
+
+            _controller = new AuthController(
+                _authServiceMock.Object,
+                _validatorMock.Object,
+                _loggerMock.Object);
+        }
+
+
+    }
+}
